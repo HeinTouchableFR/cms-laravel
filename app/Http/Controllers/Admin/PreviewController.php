578d1d70dcfbdc0ca965662715a39d77c32e23f3
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2023-05-12 23:28:19 +0200

    fix: erreur bloc blog-header et blogs-posts

--- app/Http/Controllers/Admin/PreviewController.php
+++ app/Http/Controllers/Admin/PreviewController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Content;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PreviewController extends Controller
 {
@@ -52,17 +53,32 @@ class PreviewController extends Controller
     {
         $content = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
 
+        $categories = DB::table('categories')
+            ->join('contents', 'categories.id', '=', 'contents.category_id')
+            ->where('contents.online', true)
+            ->select(DB::raw('categories.*, COUNT(categories.id) as count'))
+            ->groupBy('categories.id')
+            ->get();
+
+        $posts = Content::where('type', 'blog')->where('online', true)->paginate(10);
+
         if (array_is_list($content)) {
             return view('admin.preview.template', [
                 'blocs' => $content,
+                'title' => 'Blog',
+                'posts' => $posts,
                 'menu' => route('home'),
+                'categories' => $categories,
             ]);
         }
 
         return view(theme().'.views.'.$content['_name'], [
             'bloc' => $content,
+            'title' => 'Blog',
+            'posts' => $posts,
             'animate' => 'no-animate',
             'menu' => route('home'),
+            'categories' => $categories,
         ]);
     }
 }
