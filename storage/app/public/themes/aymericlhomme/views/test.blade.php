439057121a7d55e50aab35c8ce6faa8b61ad90e3
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2023-05-30 00:23:22 +0200

    update test

--- storage/app/public/themes/aymericlhomme/views/test.blade.php
+++ storage/app/public/themes/aymericlhomme/views/test.blade.php
@@ -20,10 +20,10 @@
                     <h3 class="h3 bold text-left m-bottom-3"
                         style="color: {{ $bloc['titleColor'] ?: "var(--contrast)" }};">{{ $item['title'] }}</h3>
                     @foreach($item['sub_items'] as $sub)
-                        <p>
+                        <div style="display: flex; gap: 8px;">
                             [{{ $sub['isTest'] ? 'X' : '  ' }}]
                             {!! $sub['content'] !!}
-                        </p>
+                        </div>
                         <div class="m-top-2" style="color: var(--color); font-size: 0.8em;">
                             {!! $sub['comment'] !!}
                         </div>
