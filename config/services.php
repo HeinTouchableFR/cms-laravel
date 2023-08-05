4e9da4f36ed29d47a5344eb213bc5577de1d5c1d
Lhomme Aymeric <37447949+HeinTouchableFR@users.noreply.github.com>
2023-08-05 03:50:26 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -34,13 +34,19 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => env('FACEBOOK_CLIENT_REDIRECT'),
+        'redirect' => '/oauth/facebook/callback',
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('GOOGLE_CLIENT_REDIRECT'),
+        'redirect' => '/oauth/google/callback',
+    ],
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => '/oauth/github/callback',
     ],
 
 ];
